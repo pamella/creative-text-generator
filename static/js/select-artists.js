9ceb43a15af19e1cbaa190984141d4e66d9056fb
pamella <pammbsilva@gmail.com>
2020-11-19 22:32:38 -0300

    Improve select artists page behavior

---
+++ static/js/select-artists.js
@@ -0,0 +1,47 @@
+const ARTISTS_SELECTED = []
+
+const artists = document.querySelectorAll('.artists-wrapper .list .item');
+const overviewContainer = document.querySelector('.overview-container');
+const overviewContainerPhotos = document.querySelectorAll('.overview-container .photo');
+const overviewContainerNames = document.querySelector('.overview-container .names');
+const generateLink = document.querySelector('.overview-container .link');
+const rootLink = generateLink.getAttribute('href');
+
+artists.forEach(artist => {
+	artist.addEventListener('click', () => handleArtistClick(artist));
+});
+
+const handleArtistClick = (artist) => {
+	const photo = artist.querySelector('.photo');
+
+	if (ARTISTS_SELECTED.includes(artist)) {
+		removeAllElements(ARTISTS_SELECTED, artist);
+		photo.style.border = null;
+		overviewContainer.style.display = 'none';
+		generateLink.setAttribute('href', `${rootLink}`);
+		return;
+	}
+
+	if(ARTISTS_SELECTED.length !== 2) {
+		ARTISTS_SELECTED.push(artist);
+		photo.style.border = '5px solid white';
+	}
+
+	if (ARTISTS_SELECTED.length === 2) {
+		overviewContainerPhotos[0].style.backgroundImage = ARTISTS_SELECTED[0].querySelector('.photo').style.backgroundImage;
+		overviewContainerPhotos[1].style.backgroundImage = ARTISTS_SELECTED[1].querySelector('.photo').style.backgroundImage;
+		overviewContainerNames.innerHTML = `${ARTISTS_SELECTED[0].querySelector('.name').innerHTML} & ${ARTISTS_SELECTED[1].querySelector('.name').innerHTML}`;
+		overviewContainer.style.display = 'flex';
+		generateLink.setAttribute('href', `${rootLink}?artists_names=${ARTISTS_SELECTED[0].getAttribute('id')}&artists_names=${ARTISTS_SELECTED[1].getAttribute('id')}`);
+	}
+
+	return;
+};
+
+const removeAllElements = (array, elem) => {
+	var index = array.indexOf(elem);
+	while (index > -1) {
+		array.splice(index, 1);
+		index = array.indexOf(elem);
+	}
+};

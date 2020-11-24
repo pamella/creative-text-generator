dba656fba08bd0e510aebd2eb0884800a6c415b0
pamella <pammbsilva@gmail.com>
2020-11-23 22:52:14 -0300

    Update settings

--- creative_text_generator/settings.py
+++ creative_text_generator/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-# import django_heroku
-# from decouple import config
+import django_heroku
+from decouple import config
 
 from pathlib import Path
 
@@ -23,17 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret'
+SECRET_KEY = config('SECRET_KEY', default='secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True)
 
 ALLOWED_HOSTS = [
     '.localhost',
     '127.0.0.1',
     '.ngrok.io',
     '.herokuapp.com',
-
 ]
 
 
@@ -130,10 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
